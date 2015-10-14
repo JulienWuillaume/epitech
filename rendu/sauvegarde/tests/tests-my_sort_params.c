8aaa6e4238d2ab64fa3bcf6d7d371895a1f7a913
wuilla_j <julien.wuillaume@epitech.eu>
2015-10-14 11:24:16 +0200

    rm rendu

--- rendu/sauvegarde/tests/tests-my_sort_params.c
+++
@@ -1,9 +0,0 @@
-/*
-** tests-my_sort_params.c for tests-my_sort_params in /home/wuilla_j/rendu/Piscine_C_J06/tests
-** 
-** Made by Julien Wuillaume
-** Login   <wuilla_j@epitech.net>
-** 
-** Started on  Tue Oct  6 18:14:12 2015 Julien Wuillaume
-** Last update Tue Oct  6 18:14:17 2015 Julien Wuillaume
-*/
